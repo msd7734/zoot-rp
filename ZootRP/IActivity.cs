47de27f90a15e32d2933689553c8121684f7105c
Matt Dennis <msd7734@rit.edu>
2016-10-05 19:51:57 -0400

    Use ScheduleWidget instead

--- ZootRP/IActivity.cs
+++ ZootRP/IActivity.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ScheduleWidget.ScheduledEvents;
+
 namespace ZootRP.Core
 {
     public interface IActivity
     {
-
+        Schedule Schedule { get; }
     }
 }
